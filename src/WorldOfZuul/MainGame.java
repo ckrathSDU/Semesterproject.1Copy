bb238d8889e16c359c2fda929331e46929d11a12
chkra19 <chkra19@student.sdu.dk>
2022-05-08 11:08:31 +0200

    Files

---
+++ src/WorldOfZuul/MainGame.java
@@ -0,0 +1,11 @@
+package WorldOfZuul;
+
+public class MainGame {
+
+    /*public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+
+    }
+     */
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import WorldOfZuul.Game;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/sample.fxml"));
+        primaryStage.setTitle("World of Desert");
+
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+
+        Game game = new Game();
+        
+    }
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
